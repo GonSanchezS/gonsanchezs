259ab79c3e26196b4bbba6639ba63ab4fadc75f0
Gonzalo <gonsanchezs@gmail.com>
2016-10-03 11:06:21 +0000

    Improved styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Gonsanchezs::Application.routes.draw do
   root 'static_pages#index'
+  get 'cities', to: 'static_pages#cities'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
